d61234f9f622fa83cb42c3ee6a5c4ddb860d7b9c
Jiri (George) Lebl <jiri.lebl@gmail.com>
2021-09-21 14:19:12 -0500

    The same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuffThe same setup for def files as the diffyqs webwork stuff

---
+++ rahw/make-defs.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+for n in sec_* ; do
+	echo "openDate = 01/01/19, 0600" > "set$n.def"
+	echo "dueDate = 01/01/29, 2359" >> "set$n.def"
+	echo "answerDate = 01/01/29, 2359" >> "set$n.def"
+	echo "problemList =" >> "set$n.def"
+	for m in `ls $n/*.pg` ; do
+		echo "rahw/$m,	1" >> "set$n.def"
+	done
+done
