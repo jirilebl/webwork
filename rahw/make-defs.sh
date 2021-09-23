0cb9df7bb0a447ba839dedf80afc53608ae72870
Jiri (George) Lebl <jiri.lebl@gmail.com>
2021-09-23 18:46:41 -0500

    Rename to something other than the diffy qs homework so that I can test in the same course

--- rahw/make-defs.sh
+++ rahw/make-defs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for n in sec_* ; do
+for n in ra_* ; do
 	echo "openDate = 01/01/19, 0600" > "set$n.def"
 	echo "dueDate = 01/01/29, 2359" >> "set$n.def"
 	echo "answerDate = 01/01/29, 2359" >> "set$n.def"
