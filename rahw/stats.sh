aba4f24abbdf3630025baaff32d8a6dc2bd62755
Jiri (George) Lebl <jiri.lebl@gmail.com>
2021-10-12 17:28:10 -0500

    Add a couple of untested problems from 4.1

---
+++ rahw/stats.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+for n in ra_* ; do
+	printf '%-10s %2d exercises (%d from .in files)\n' "$n" `ls -1 $n/*.pg | wc -l` `ls -1 $n/*.in | wc -l`
+done
+
+echo
+echo Total: `ls -1 */*.pg | wc -l` exercises
+echo Total .in files: `ls -1 */*.in | wc -l` exercises
